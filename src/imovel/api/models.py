e8cf5c956217d19c87c3f56426e0410051298893
Antonio Aldisio <aldisiofilho@gmail.com>
2023-06-29 21:17:58 -0300

    update

--- src/imovel/api/models.py
+++ src/imovel/api/models.py
@@ -7,9 +7,28 @@ class Imovel(models.Model):
     quantidade_banheiros = models.IntegerField()
     aceita_animais = models.BooleanField()
     valor_limpeza = models.DecimalField(max_digits=8, decimal_places=2)
-    data_ativacao = models.DateField(auto_now_add=False)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    data_atualizacao = models.DateTimeField(auto_now=True)
+    data_ativacao = models.DateField()
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True)
+    data_atualizacao = models.DateTimeField(auto_now=True, null=True)
 
-    def __str__(self):
-        return self.codigo_imovel
+
+class Anuncio(models.Model):
+    codigo_imovel = models.IntegerField()
+    plataforma = models.CharField(max_length=20)
+    taxa_plataforma = models.DecimalField(max_digits=5, decimal_places=2)
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True)
+    data_atualizacao = models.DateTimeField(auto_now=True, null=True)
+    imovel = models.ForeignKey(Imovel, on_delete=models.CASCADE)
+
+
+class Reserva(models.Model):
+    codigo_reserva = models.CharField(max_length=10)
+    id_anuncio = models.IntegerField()
+    data_checkin = models.DateField()
+    data_checkout = models.DateField()
+    preco_total = models.DecimalField(max_digits=8, decimal_places=2)
+    comentario = models.TextField(blank=True, null=True)
+    numero_hospedes = models.IntegerField()
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True)
+    data_atualizacao = models.DateTimeField(auto_now=True, null=True)
+    anuncio = models.ForeignKey(Anuncio, on_delete=models.CASCADE)
--- src/imovel/start.sh
+++ src/imovel/start.sh
@@ -2,15 +2,15 @@
 echo "Irei esperar 60 segundos"
 sleep 60
 
-echo "Iniciando migrate"
+python manage.py makemigrations
 python manage.py migrate api
 
-if [ $? -eq 0 ]; then
-    echo "Iniciando loaddata"
-    python manage.py loaddata seeder.json
-else
-    echo "Migrate api com erro."
-fi
+echo "Iniciando loaddata"
+python manage.py loaddata seeder.json
 
+if [ $? -eq 0 ]; then
 echo "Inicaindo API"
 python manage.py runserver 0.0.0.0:8000
+else
+    echo "Erro ao iniciar API"
+fi
