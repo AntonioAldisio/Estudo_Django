5fa10571ac284c5d2d9a14a2589638b0a8315055
Antonio Aldisio <aldisiofilho@gmail.com>
2023-07-01 15:33:54 -0300

    fix

--- src/reservas/api/views.py
+++ src/reservas/api/views.py
@@ -1,19 +1,20 @@
 import random
 import string
 
-from rest_framework import viewsets, filters, status
+from rest_framework import viewsets, filters
 from drf_yasg.utils import swagger_auto_schema
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Reserva
 from .serializers import ReservasSerializer
-from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 
 
 class ReservaViewSet(viewsets.ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = ReservasSerializer
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter, DjangoFilterBackend]
+    filter_backends = [filters.OrderingFilter,
+                       filters.SearchFilter,
+                       DjangoFilterBackend]
     search_fields = '__all__'
     filterset_fields = '__all__'
     ordering_fields = '__all__'
@@ -24,15 +25,26 @@ class ReservaViewSet(viewsets.ModelViewSet):
         code = ''.join(random.choice(characters) for _ in range(length))
         return code
 
+    @swagger_auto_schema(responses={200: ReservasSerializer()})
     def create(self, request, *args, **kwargs):
-        checkin_date = request.data.get('checkin_date')
-        checkout_date = request.data.get('checkout_date')
+        checkin_date = request.data.get('data_checkin')
+        checkout_date = request.data.get('data_checkout')
+        id_anuncio = request.data.get('id_anuncio')
 
-        if checkin_date and checkout_date:
-            if checkin_date > checkout_date:
-                raise ValidationError("A data de check-in não pode ser posterior à data de check-out.")
+        if checkin_date and checkout_date and checkin_date > checkout_date:
+            raise ValidationError("""A data de check-in não pode
+                                     ser posterior à data de check-out.""")
 
-        request.data['reservation_code'] = self.generate_reservation_code()
+        overlapping_reservations = self.queryset.filter(
+            id_anuncio=id_anuncio,
+            data_checkin__lte=checkout_date,
+            data_checkout__gte=checkin_date
+        )
+
+        if overlapping_reservations.exists():
+            raise ValidationError("Já existe uma reserva para o mesmo anúncio neste período.")
+
+        request.data['codigo_reserva'] = self.generate_reservation_code()
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(responses={200: ReservasSerializer(many=True)})
@@ -42,10 +54,6 @@ class ReservaViewSet(viewsets.ModelViewSet):
     @swagger_auto_schema(responses={200: ReservasSerializer()})
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
-
-    @swagger_auto_schema(request_body=ReservasSerializer, responses={201: ReservasSerializer()})
-    def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(responses={204: None})
     def destroy(self, request, *args, **kwargs):
